629da18bb22733955e128ea680143bf76864fb18
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-03-21 23:15:28 +0600

    Created flutter project 02

---
+++ flutter_project_02/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeninTirkemem());
+}
+
+class MeninTirkemem extends StatelessWidget {
+  const MeninTirkemem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int san = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+        title: const Text(
+          'Flutter Course 19',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$san',
+              style: const TextStyle(fontSize: 70),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      san--;
+                    });
+                  },
+                  icon: const Icon(Icons.remove),
+                  label: const Text('Decrement'),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      san++;
+                    });
+                  },
+                  icon: const Icon(Icons.add),
+                  label: const Text('Increment'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
