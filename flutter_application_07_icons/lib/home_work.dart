80c193bfb1360236b51efdcb9a33b0186604a663
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-05-21 00:41:52 +0600

    added flutter 07 use icons

---
+++ flutter_application_07_icons/lib/home_work.dart
@@ -0,0 +1,4 @@
+/*
+1) Тапшырма 4 jasoo (Card orduna TextField)
+2) Buturuu https://www.google.com/search?sca_esv=19fb95802b09d0d0&sca_upv=1&rlz=1C5CHFA_enKG1019KG1019&q=flutter+simple+ui&tbm=isch&source=lnms&prmd=ivsmbz&sa=X&ved=2ahUKEwiL3_ap5JKGAxXMMRAIHS4jAhoQ0pQJegQIDBAB&biw=1440&bih=813&dpr=2#imgrc=m4N7NYmANgtePM
+*/
