c8e68b5dcd934f0d73c83524dbc19d6331d93640
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-05-28 19:14:36 +0600

    Added 09 Flutter xylophone

---
+++ flutter_application_09_hylophone/lib/home_work.dart
@@ -0,0 +1,4 @@
+/*
+1) Flutter Xylophone app jasoo
+2) UI jasoo https://www.google.com/search?sca_esv=668e1abf6fdfcd6c&rlz=1C5CHFA_enKG1019KG1019&sxsrf=ADLYWIIORjs-kbxBAtkjiYPXlmRh5-DPDg:1716834185895&q=flutter+simple+ui&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjBgqfeua6GAxVtLxAIHU9TDQUQ0pQJegQIDRAB&biw=1440&bih=779&dpr=2#imgrc=9-2wnjLjxL2wLM
+*/
---
+++ flutter_application_09_hylophone/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_09_hylophone/home_page.dart';
+
+// https://github.com/rodydavis/flutter_piano?tab=readme-ov-file
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
