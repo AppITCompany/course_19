c7fec9df91b3bcb8300954112e17026d193e475e
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-08-20 10:37:21 +0600

    Started chat app lesson 42

---
+++ flutter_application_42_chat_app/doc/tech.dart
@@ -0,0 +1,17 @@
+/*
+1) Mobile Flutter
+   a) animated_text_kit
+   b) firebase_core
+   c) firebase_auth
+   d) cloud_firestore
+   e) shared_preferences
+   f) bloc & flutter_bloc
+   j) json_serializable
+   k) json_annotation
+   l) build_runner
+   m) flutter_chat_ui
+
+3) Backend Firebase
+   a) firebase_auth
+   b) cloud_firestore
+*/
